c4c6d0330f1e79f8fecaa44315c7d6c47c8e5211
ZeyadShehata <zeyadelzek@gmail.com>
2021-09-12 00:29:50 +0200

    Feat: Add navigation from shoelistings fragment to shoedetails fragment through the  fab

--- app/src/main/java/com/example/android/shoeinventory/ui/ShoeListings/ShoeListingsFragment.kt
+++ app/src/main/java/com/example/android/shoeinventory/ui/ShoeListings/ShoeListingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.shoeinventory.R
 import com.example.android.shoeinventory.databinding.FragmentShoeListingsBinding
 
@@ -18,7 +19,9 @@ class ShoeListingsFragment: Fragment()
         savedInstanceState: Bundle?
     ): View? {
         val binding : FragmentShoeListingsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_listings, container, false)
-
+        binding.fab.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(ShoeListingsFragmentDirections.actionFragmentShoeListingsToShoeDetailsFragment())
+        }
         return binding.root
     }
 }
